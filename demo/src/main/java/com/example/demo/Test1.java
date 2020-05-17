2ae19f6a5c4bfb68cccad7c85a61194eb7c3ff53
dallask <dallask@dallask-Inspiron-5559>
2020-05-16 19:46:03 -0500

    commit 2

--- demo/src/main/java/com/example/demo/Test1.java
+++ demo/src/main/java/com/example/demo/Test1.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Test1 {
 	
+	private static final Logger logger = LoggerFactory.getLogger(Test1.class);
+	
 	public void getDetails() {
-		
+		logger.info("getDetails");
 	}
 
 }
